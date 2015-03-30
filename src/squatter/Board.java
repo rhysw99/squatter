9bacc18ef168ef7a4980c626f88f4055623fa67a
ajmorton <ajmorton2@gmail.com>
2015-03-31 10:53:18 +1100

    changed some info in comments.txt
    added some comments and formatting to Board and Cell classes.

--- src/squatter/Board.java
+++ src/squatter/Board.java
@@ -2,19 +2,24 @@ package squatter;
 
 import java.awt.Point;
 
+
+
 public class Board {
 	
-	private Cell[][] board;
+	private Cell[][] board;					// a 2D array of Cells, represents the game board
 	
+	
+	/* CONSTRUCTER */
 	public Board(int size) {
 		this.board = new Cell[size][size];
 	}
 	
-	public Cell getCell(Point p) {
-		return this.board[p.y][p.x];
-	}
+	/* SETTERS */
+	public void setCell(Point p, Cell c)	{ board[p.y][p.x] = c; }
 	
-	public void setCell(Point p, Cell c) {
-		board[p.y][p.x] = c;
-	}
+	/* GETTERS */
+	public Cell getCell(Point p) 			{ return this.board[p.y][p.x]; }
+	
+	
+
 }
--- src/squatter/Cell.java
+++ src/squatter/Cell.java
@@ -4,23 +4,21 @@ import java.awt.Point;
 
 public class Cell {
 	
-	private char owner;
-	private Point p;
 	
+	private char owner;					// the owner of the cell ('B', 'W', '+', '-')
+	private Point p;					// the x and y coordinates of the cell (as a Point)
+	
+	
+	/* CONSTRUCTER */
 	public Cell(Point p, char owner) {
 		this.p = p;
 		this.owner = owner;
-		
-	}
-	public void setOwner(char owner) {
-		this.owner = owner;
 	}
 	
-	public char getOwner(){
-		return this.owner;
-	}
+	/* SETTERS */
+	public void setOwner(char owner)	{ this.owner = owner; }	
 	
-	public Point getPosition() {
-		return p;
-	}
+	/* GETTERS */
+	public char getOwner()				{ return this.owner; }
+	public Point getPosition() 			{ return p; }
 }
