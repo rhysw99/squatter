30be8625108a7ba3433e73f28b2bf9ab715e96ef
Rhys Williams <rhyswilliams314@gmail.com>
2015-04-01 11:15:37 +1100

    Final final commit

--- src/squatter/Board.java
+++ src/squatter/Board.java
@@ -1,3 +1,10 @@
+/**
+ * COMP30024 Artificial Intelligence
+ * Project A - Checking Win States
+ * ajmorton Andrew Morton 522139 
+ * rhysw    Rhys Williams 661561
+ */
+
 package squatter;
 
 import java.awt.Point;
